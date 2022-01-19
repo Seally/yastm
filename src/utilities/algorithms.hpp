f203fdb288e34b1d8fc5a33e0f61bd8e8d1202dd
Porama Ignoi Ruengrairatanaroj <seal.corner@gmail.com>
2022-01-20 01:26:23 +0700

    Add KeyEqual template argument.

--- src/utilities/algorithms.hpp
+++ src/utilities/algorithms.hpp
@@ -7,13 +7,15 @@
 template <
     typename iterator,
     typename Hash =
-        std::hash<typename std::iterator_traits<iterator>::value_type>>
+        std::hash<typename std::iterator_traits<iterator>::value_type>,
+    typename KeyEqual =
+        std::equal_to<typename std::iterator_traits<iterator>::value_type>>
 bool areAllUnique(iterator begin, iterator end)
 {
     using T = std::iterator_traits<iterator>::value_type;
 
-    // Use an unordered set to disambiguate between FormIds.
-    std::unordered_set<std::reference_wrapper<const T>, Hash> uniques;
+    // Use an unordered set to disambiguate between elements.
+    std::unordered_set<std::reference_wrapper<const T>, Hash, KeyEqual> uniques;
 
     std::size_t count = 0;
 
