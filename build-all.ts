b586b684aa2b0d8b205e6874b5f63f4c1d36cab5
Porama Ignoi Ruengrairatanaroj <seal.corner@gmail.com>
2022-02-02 21:08:04 +0700

    Add more configurations to the build-all.ts script.

--- build-all.ts
+++ build-all.ts
@@ -1,12 +1,54 @@
+import * as flags from "https://deno.land/std@0.123.0/flags/mod.ts";
+import * as t from "./scripts/typanion.ts";
+
 import cmakeUserPresets from "./CMakeUserPresets.json" assert { type: "json" };
+
+const args = flags.parse(Deno.args, {
+    alias: {
+        help: "h",
+    },
+    boolean: ["help", "rebuild", "clean-only", "configure-only"],
+});
+
+function printHelp() {
+    console.log([
+        "OPTIONS",
+        "--help, -h        Prints this help.",
+        "--rebuild         Cleans and builds the project.",
+        "--clean-only      Only clean the targets. Prevents CMake configure.",
+        "--configure-only  Do not compile, only configure CMake.",
+        "",
+        "In case of conflicting arguments, the priority is in this order:",
+        "",
+        "    --clean-only > --configure-only > --rebuild",
+    ].join("\n"));
+}
+
+const verifyArgs = t.isPartial({
+    help: t.isBoolean(),
+    h: t.isBoolean(),
+    rebuild: t.isBoolean(),
+    "configure-only": t.isBoolean(),
+    "clean-only": t.isBoolean(),
+});
+
+if (!verifyArgs(args)) {
+    printHelp();
+    Deno.exit(1);
+}
+
+if (args.help) {
+    printHelp();
+    Deno.exit();
+}
 
 let index = 0;
 
 for (const buildPreset of cmakeUserPresets?.buildPresets) {
     const { name, configurePreset } = buildPreset;
 
-    {
-        const displayString = `${configurePreset} (configure - task ${
+    if (args["clean-only"]) {
+        const displayString = `${configurePreset} (clean - task ${
             index + 1
         } of ${cmakeUserPresets.buildPresets.length})`;
 
@@ -15,7 +57,7 @@ for (const buildPreset of cmakeUserPresets?.buildPresets) {
         console.log("=".repeat(displayString.length));
 
         const process = Deno.run({
-            cmd: ["cmake", "--preset", configurePreset],
+            cmd: ["cmake", "--build", "--target", "clean", "--preset", name],
         });
 
         const status = await process.status();
@@ -23,25 +65,50 @@ for (const buildPreset of cmakeUserPresets?.buildPresets) {
         if (!status.success) {
             Deno.exit(1);
         }
-    }
+    } else {
+        {
+            const displayString = `${configurePreset} (configure - task ${
+                index + 1
+            } of ${cmakeUserPresets.buildPresets.length})`;
 
-    {
-        const displayString = `${configurePreset} (build - task ${
-            index + 1
-        } of ${cmakeUserPresets.buildPresets.length})`;
+            console.log("=".repeat(displayString.length));
+            console.log(displayString);
+            console.log("=".repeat(displayString.length));
 
-        console.log("=".repeat(displayString.length));
-        console.log(displayString);
-        console.log("=".repeat(displayString.length));
+            const process = Deno.run({
+                cmd: ["cmake", "--preset", configurePreset],
+            });
 
-        const process = Deno.run({
-            cmd: ["cmake", "--build", "--preset", name],
-        });
+            const status = await process.status();
 
-        const status = await process.status();
+            if (!status.success) {
+                Deno.exit(1);
+            }
+        }
 
-        if (!status.success) {
-            Deno.exit(1);
+        if (!args["configure-only"]) {
+            const displayString = `${configurePreset} (build - task ${
+                index + 1
+            } of ${cmakeUserPresets.buildPresets.length})`;
+
+            console.log("=".repeat(displayString.length));
+            console.log(displayString);
+            console.log("=".repeat(displayString.length));
+
+            const runArgs = ["cmake", "--build"];
+
+            if (args["rebuild"]) {
+                runArgs.push("--clean-first");
+            }
+
+            runArgs.push("--preset", name);
+
+            const process = Deno.run({ cmd: runArgs });
+            const status = await process.status();
+
+            if (!status.success) {
+                Deno.exit(1);
+            }
         }
     }
 
