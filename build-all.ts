03150328de4f2459c93fe7b885af15624951680a
Porama Ignoi Ruengrairatanaroj <seal.corner@gmail.com>
2022-12-30 06:37:31 +0700

    Make build-all.ts filter out non-copying targets by default.

--- build-all.ts
+++ build-all.ts
@@ -7,13 +7,26 @@ const args = flags.parse(Deno.args, {
     alias: {
         help: "h",
     },
-    boolean: ["help", "rebuild", "skip-configure", "skip-build", "clean-only"],
+    boolean: [
+        "help",
+        "all",
+        "list-only",
+        "rebuild",
+        "skip-configure",
+        "skip-build",
+        "clean-only",
+    ],
 });
 
 function printHelp() {
     console.log([
         "OPTIONS",
         "--help, -h        Prints this help.",
+        "--all             Build all presets in CMakeUserPresets.json. If this is not",
+        "                  specified, all build presets that do not have COPY_BUILD",
+        "                  enabled will be excluded",
+        "--list-only       List the presets that the script will process given the",
+        "                  current arguments (had this flag not been specified).",
         "--rebuild         Cleans and builds the project.",
         "--skip-configure  Skip CMake configure step.",
         "--skip-build      Skip building step.",
@@ -21,13 +34,15 @@ function printHelp() {
         "",
         "In case of conflicting arguments, the priority is in this order:",
         "",
-        "    clean-only > skip-configure == skip-build > rebuild",
+        "    help > list-only > clean-only > skip-configure == skip-build > rebuild",
     ].join("\n"));
 }
 
 const verifyArgs = t.isPartial({
     help: t.isBoolean(),
     h: t.isBoolean(),
+    all: t.isBoolean(),
+    "list-only": t.isBoolean(),
     rebuild: t.isBoolean(),
     "skip-configure": t.isBoolean(),
     "skip-build": t.isBoolean(),
@@ -44,41 +59,73 @@ if (args.help) {
     Deno.exit();
 }
 
-let index = 0;
+function getBuildPresets() {
+    let presets = cmakeUserPresets?.buildPresets ?? [];
 
-for (const buildPreset of cmakeUserPresets?.buildPresets) {
-    const { name, configurePreset } = buildPreset;
+    if (!args.all) {
+        presets = presets.filter((preset) => {
+            const configurePresetName = preset.configurePreset;
 
-    if (args["clean-only"]) {
-        const displayString = `${configurePreset} (clean - task ${
-            index + 1
-        } of ${cmakeUserPresets.buildPresets.length})`;
+            const configurePreset = cmakeUserPresets?.configurePresets.find(
+                (preset) => {
+                    return preset.name === configurePresetName;
+                },
+            );
 
-        console.log("=".repeat(displayString.length));
-        console.log(displayString);
-        console.log("=".repeat(displayString.length));
-
-        const process = Deno.run({
-            cmd: ["cmake", "--build", "--target", "clean", "--preset", name],
+            return !!configurePreset?.cacheVariables?.COPY_BUILD;
         });
+    }
 
-        const status = await process.status();
+    return presets;
+}
 
-        if (!status.success) {
-            Deno.exit(1);
-        }
-    } else {
-        if (!args["skip-configure"]) {
-            const displayString = `${configurePreset} (configure - task ${
-                index + 1
-            } of ${cmakeUserPresets.buildPresets.length})`;
+function printBanner(message: string, borderCharacter = "=") {
+    if (borderCharacter.length > 1) {
+        throw new Error(
+            `borderCharacter "${borderCharacter}" is longer than one character`,
+        );
+    }
 
-            console.log("=".repeat(displayString.length));
-            console.log(displayString);
-            console.log("=".repeat(displayString.length));
+    const border = borderCharacter.repeat(message.length);
+
+    console.log(border);
+    console.log(message);
+    console.log(border);
+}
+
+const buildPresets = getBuildPresets();
+
+if (buildPresets.length <= 0) {
+    console.log("No build presets available given the current arguments.");
+} else if (args["list-only"]) {
+    console.log(
+        `The following ${buildPresets.length} presets will be processed:`,
+    );
+
+    for (let index = 0; index < buildPresets.length; ++index) {
+        console.log(`[${index + 1}] ${buildPresets[index].name}`);
+    }
+} else {
+    for (let index = 0; index < buildPresets.length; ++index) {
+        const buildPreset = buildPresets[index];
+        const { name, configurePreset } = buildPreset;
+
+        if (args["clean-only"]) {
+            printBanner(
+                `${configurePreset} (clean - task ${
+                    index + 1
+                } of ${buildPresets.length})`,
+            );
 
             const process = Deno.run({
-                cmd: ["cmake", "--preset", configurePreset],
+                cmd: [
+                    "cmake",
+                    "--build",
+                    "--target",
+                    "clean",
+                    "--preset",
+                    name,
+                ],
             });
 
             const status = await process.status();
@@ -86,33 +133,47 @@ for (const buildPreset of cmakeUserPresets?.buildPresets) {
             if (!status.success) {
                 Deno.exit(1);
             }
-        }
+        } else {
+            if (!args["skip-configure"]) {
+                printBanner(
+                    `${configurePreset} (configure - task ${
+                        index + 1
+                    } of ${buildPresets.length})`,
+                );
 
-        if (!args["skip-build"]) {
-            const displayString = `${configurePreset} (build - task ${
-                index + 1
-            } of ${cmakeUserPresets.buildPresets.length})`;
+                const process = Deno.run({
+                    cmd: ["cmake", "--preset", configurePreset],
+                });
 
-            console.log("=".repeat(displayString.length));
-            console.log(displayString);
-            console.log("=".repeat(displayString.length));
+                const status = await process.status();
 
-            const runArgs = ["cmake", "--build"];
-
-            if (args["rebuild"]) {
-                runArgs.push("--clean-first");
+                if (!status.success) {
+                    Deno.exit(1);
+                }
             }
 
-            runArgs.push("--preset", name);
+            if (!args["skip-build"]) {
+                printBanner(
+                    `${configurePreset} (build - task ${
+                        index + 1
+                    } of ${buildPresets.length})`,
+                );
 
-            const process = Deno.run({ cmd: runArgs });
-            const status = await process.status();
+                const runArgs = ["cmake", "--build"];
 
-            if (!status.success) {
-                Deno.exit(1);
+                if (args["rebuild"]) {
+                    runArgs.push("--clean-first");
+                }
+
+                runArgs.push("--preset", name);
+
+                const process = Deno.run({ cmd: runArgs });
+                const status = await process.status();
+
+                if (!status.success) {
+                    Deno.exit(1);
+                }
             }
         }
     }
-
-    ++index;
 }
